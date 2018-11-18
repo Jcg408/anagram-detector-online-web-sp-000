ebe39efd380ecdc9963ff601cd9761170b761393
Jessica Gladstone <jcg321@comcast.net>
2018-11-18 21:50:46 +0000

    automatically backed up by learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,19 @@
-# Your code goes here!
+
+class Anagram
+    attr_accessor :word
+      def initialize (word)
+        @word = word
+    end   
+    
+    def match(arr)
+        wordAnagram = []
+        arr.map do |words|
+         words.split(" ")
+          if words.chars.sort  == self.word.chars
+         wordAnagram << words
+          end  
+        end
+        wordAnagram
+      end
+    end
+    
